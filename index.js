f376c078b3929d328a57fd4d88b905baa6b42e2e
prastowoadi18 <prastowoadiworkv@gmail.com>
2023-05-25 11:20:52 +0700

    add origin url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://weeding-guestlist-vd73.vercel.app",
+      "https://wedding-guestlist.vercel.app/",
     ],
     credentials: true,
   })
